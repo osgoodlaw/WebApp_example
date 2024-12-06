6eb99ab252263d15dd7455f7c781a5c5de0cb34b
osgoodlaw <finlay.p.osgood@gmail.com>
2024-12-06 19:45:43 +0000

    initial commit

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired
+
+class IdeaForm(FlaskForm):
+    idea = TextAreaField('idea', validators=[DataRequired()])
